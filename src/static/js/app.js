47404c04f0a5b0f7727f5483f09ce27f50e346dc
Med23B <simobiya1995@gmal.com>
2024-06-05 00:40:23 +0200

    Second try on CI/CD HHHH

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-               <p className="text-center">You have no todo items yet! Add one above!</p>
+               <p className="text-center">You have no todo items yet! Add one above! V2 CI/CD second try hhhh</p>
             )}
             {items.map(item => (
                 <ItemDisplay
